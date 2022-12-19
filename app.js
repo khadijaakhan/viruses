f6451a2e2eecb5c0c641d5c59e50f8fbba2864bb
whitedevil69 <wajeehwajeeh26@gmail.com>
2022-12-19 12:47:03 +0500

    commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// var nav = document.querySelector('nav')
+// window.addEventListener('scroll', function(){
+//     if(this.window.pageYOffset > 1){
+//         nav.classList.add('bg-danger');
+//     }else{
+//         nav.classList.remove('bg-danger');
+//     }
+// })
